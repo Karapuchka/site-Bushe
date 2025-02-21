2ac58f17d265036e426735b40b2e8deb46dfd80d
Karapuchka <karapu4ka2@gmail.com>
2025-02-21 10:08:28 +0300

    .

---
+++ app.js
@@ -0,0 +1,30 @@
+import express, { query } from 'express';
+import fs from 'fs';
+import mysql from 'mysql2';
+import path from 'path';
+
+const app = express();
+
+let user; //Инфомрация о пользователе
+
+//Настройка подключения к бд
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 3000,
+    user: 'root',
+    database: 'sweetStore',
+});
+
+//Создание парсера
+const urlcodedParsers = express.urlencoded({extended: false});
+
+//JSON парсер
+const JSONParser = express.json();
+
+//Указание пути к файлом hbs
+app.use(express.static(path.join(fs.realpathSync('.') + '/public')));
+app.set('view engine', 'hbs');
+
+app.listen(3000, ()=>{
+    return console.log('Server ative. URL: http://localhost:3000/');
+});
