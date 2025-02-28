5781b8da53d63db2369828f6497b58df89438ff7
Артём Кравец <karapu4ka2@gmail.com>
2025-02-28 20:21:45 +0300

    -

--- app.js
+++ app.js
@@ -35,10 +35,63 @@ app.get('/', (_, res)=>{
         for (let i = 0; i < dataFood.length; i++) {
             arr.push({
                 'title': dataFood[i].title,
-            })            
+            });            
         }
         return res.render('index.hbs', {
             'search-list': arr,
+        });
+    });
+});
+
+app.get('/product', (_, res)=>{
+    pool.query('SELECT * FROM foodStuff', (errFood, dataFood)=>{
+        if(errFood) return console.log(errFood);
+
+        let listFoods, listSearch = [];
+        let countFoods = dataFood.length;
+        
+        for (let i = 0; i < dataFood.length; i++) {
+            listFoods.push({
+                'title': dataFood[i].title,
+                'icon': dataFood[i].img,
+                'price': dataFood[i].cost,
+            });
+
+            listSearch.push({
+                'title': dataFood[i].title,
+            });        
+        };
+
+        return res.render('index.hbs', {
+            'search-list': listSearch,
+            'countFoods': countFoods,
+            'productFoods': listFoods,
+        });
+    });
+});
+
+app.get('/opt-product', (_,res)=>{
+    pool.query('SELECT * FROM foodStuff', (errFood, dataFood)=>{
+        if(errFood) return console.log(errFood);
+
+        let listFoods, listSearch = [];
+        
+        for (let i = 0; i < dataFood.length; i++) {
+            if(dataFood[i].type == 'bread'){
+                listFoods.push({
+                    'title': dataFood[i].title,
+                    'icon': dataFood[i].img,
+                });
+
+                listSearch.push({
+                    'title': dataFood[i].title,
+                });   
+            };
+        };
+
+        return res.render('index.hbs', {
+            'search-list': listSearch,
+            'productFoods': listFoods,
         });
     });
 });
